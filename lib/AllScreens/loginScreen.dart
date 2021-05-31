86d64c4086f03189fe89f59573a9cf04f8e5c9bd
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-05-31 17:18:22 +0200

    Initial commit

---
+++ lib/AllScreens/loginScreen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(height: 45.0),
+              Image(
+                image: AssetImage("images/logo.png"),
+                width: 350.0,
+                height: 350.0,
+                alignment: Alignment.center,
+              )
+
+              SizedBox(height: 1.0,),
+              Text(
+                "Login as a Rider",
+                style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                textAlign: TextAlign.center,
+              ),
+
+              Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Column(
+                  Children: [
+
+                    SizedBox(height: 1.0,),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          colors: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    style: TextStyle(fontSize: 14.0),
+                  ),
+
+                    SizedBox(height: 1.0,),
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(
+                          fontSize: 14.0,
+                        ),
+                        hintStyle: TextStyle(
+                          colors: Colors.grey,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                      style: TextStyle(fontSize: 14.0),
+                    ),
+
+                    SizedBox(height: 1.0,),
+                    RaisedButton(
+                      color: Colors.yellow,
+                      textColor: Colors.white,
+                      child: Container(
+                        height: 50.0,
+                        child: Center(
+                          child: Text(
+                            "Login",
+                            style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold"),
+                          ),
+                        ),
+                      ),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(24.0),
+                      ),
+                      onPressed: ()
+                        {
+                          print("loggedin button clicked");
+                        },
+                    ),
+
+                  ],
+                ),
+              ),
+
+              FlatButton(
+                onPressed: ()
+                {
+                  print("clicked");
+                },
+                child: Text(
+                  "Do not have an Account? Register Here.",
+                ),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
---
+++ lib/AllScreens/registrationScreen.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+
+
+class RegistrationScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(height: 20.0),
+                Image(
+                  image: AssetImage("images/logo.png"),
+                  width: 350.0,
+                  height: 350.0,
+                  alignment: Alignment.center,
+                )
+
+                SizedBox(height: 1.0,),
+                Text(
+                  "Register as a Rider",
+                  style: TextStyle(fontSize: 24.0, fontFamily: "Brand Bold"),
+                  textAlign: TextAlign.center,
+                ),
+
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    Children: [
+
+                      SizedBox(height: 1.0,),
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                          labelText: "Name",
+                          labelStyle: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                          hintStyle: TextStyle(
+                            colors: Colors.grey,
+                            fontSize: 10.0,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+
+                      SizedBox(height: 1.0,),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          labelText: "Email",
+                          labelStyle: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                          hintStyle: TextStyle(
+                            colors: Colors.grey,
+                            fontSize: 10.0,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+
+                      SizedBox(height: 1.0,),
+                      TextFormField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          labelText: "Phone",
+                          labelStyle: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                          hintStyle: TextStyle(
+                            colors: Colors.grey,
+                            fontSize: 10.0,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+
+                      SizedBox(height: 1.0,),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: "Password",
+                          labelStyle: TextStyle(
+                            fontSize: 14.0,
+                          ),
+                          hintStyle: TextStyle(
+                            colors: Colors.grey,
+                            fontSize: 10.0,
+                          ),
+                        ),
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+
+                      SizedBox(height: 1.0,),
+                      RaisedButton(
+                        color: Colors.yellow,
+                        textColor: Colors.white,
+                        child: Container(
+                          height: 50.0,
+                          child: Center(
+                            child: Text(
+                              "Create Account",
+                              style: TextStyle(fontSize: 18.0, fontFamily: "Brand Bold"),
+                            ),
+                          ),
+                        ),
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(24.0),
+                        ),
+                        onPressed: ()
+                        {
+                          print("loggedin button clicked");
+                        },
+                      ),
+
+                    ],
+                  ),
+                ),
+
+                FlatButton(
+                  onPressed: ()
+                  {
+                    print("clicked");
+                  },
+                  child: Text(
+                    "Already have an Account? Login Here.",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
