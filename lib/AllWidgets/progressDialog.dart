b2b455f4d387ac25787f206ce9c0ee4a2e91edac
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-06-02 22:00:14 +0200

    Dialog/Padding

--- lib/AllWidgets/progressDialog.dart
+++ lib/AllWidgets/progressDialog.dart
@@ -1,3 +1,35 @@
 import 'package:flutter/material.dart';
 
 
+class ProgressDialog extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Dialog(
+      backgroundColor: Colors.yellow,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(6.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              SizedBox(width: 6.0),
+              CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Colors>(Colors.black),),
+              SizedBox(width: 26.0),
+              Text(
+                message,
+                style: TextStyle(color: Colors.black, fontSize: 10.0),
+               ),
+              ],
+            ),
+          ),
+        ),
+      };
+   }
+}
