53b211be637aeb1756366aa43084111762370881
Doug Rush <71516868+rushdm15@users.noreply.github.com>
2021-06-04 14:55:47 +0200

    navigation drawer

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -31,6 +31,50 @@ class _MainScreenState extends State<MainScreen>
       appBar: AppBar(
         title: Text("Main Screen"),
       ),
+      drawer: Container(
+        color: Colors.white,
+        width: 255.0,
+        child: Drawer(
+          children: [
+            Container(
+              height: 165.0,
+              child: DrawerHeader(
+                decoration: BoxDecoration(color: Colors.white),
+                child: Row(
+                  children: [
+                    Image.asset("images/user_icon.png", height: 65.0, width: 65.0,),
+                    SizedBox(width: 16.0,),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Profile Name", style: TextStyle(fontSize: 16.0, fontFamily: "Brand-Bold"),),
+                        SizedBox(height: 6.0,),
+                        Text("Visit Profile"),
+                      ],
+                    )
+                  ],
+                ),
+                
+              ),
+            ),
+
+            DividerWidget(),
+
+            SizedBox(height: 12.0,),
+
+            ListTile(
+              leading: Icon(Icons.history),
+              title: Text("History", style: TextStyle(fontSize: 15.0),),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Visit Profile", style: TextStyle(fontSize: 15.0),),
+            ),ListTile(
+              leading: Icon(Icons.info),
+              title: Text("About", style: TextStyle(fontSize: 15.0),),
+          ],
+        ),
+      ),
       body: Stack(
         children: [
           GoogleMap(
@@ -42,8 +86,92 @@ class _MainScreenState extends State<MainScreen>
               _controllerGoogleMap.complete(controller);
               newGoogleMapController = controller;
             }
+          ),
 
+          //HamburgerButton for Drawer
+          Positioned(
+            top: 45.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap: ()
+                {
+                  scaffoldKey.currentState.openDrawer();
+                },
+              child: Container()
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 6.0,
+                      spreadRadius: .5,
+                      offset: Offset(
+                        .7,
+                        .7,
+                      ),
+                    ),
+                  ],
+                ),
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.menu, color: Colors.black,),
+                radius: 20.0,
+              ),
+            ),
           )
+
+          Positioned(
+            left: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 320.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 16.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 6.0),
+                    Text("Hi there,", style: TextStyle(fontSize: 12.0),),
+                    Text("Where to?", style: TextStyle(fontSize: 20.0, fontFamily: "Brand-Bold"),),
+                    SizedBox(height: 20.0),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 6.0,
+                            spreadRadius: 0.5,
+                            offset: Offset(0.7, 0.7),
+                          ),
+                        ],
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.search, color: Colors.blueAccent,),
+                            SizedBox(width: 10.0,),
+                            Text("Search Drop Off")
+                          ],
+                        ),
+                      ),
+                    ),
+
                     SizedBox(height: 24.0),
                     Row(
                       children: [
